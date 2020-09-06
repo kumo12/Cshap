73a00052a663502c5df54b62ef80567d6f0128fa
30302 고유빈 <63216449+kumo12@users.noreply.github.com>
2020-09-07 01:02:27 +0900

    Add files via upload

--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -2,11 +2,38 @@
 
 namespace AbstractClass
 {
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+        //추상메소드
+        public abstract void AbstractMethodA();
+    }
+    class Derived : AbstractBase
+    {
+        //      public void PrivateMethodA(){
+        //          Console.WriteLine("PrivateMethodA()");
+        //      }
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+            PrivateMethodA();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+            //  obj.PrivateMethodA();
         }
     }
 }
